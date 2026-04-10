89d750fb439a39acab0d80bb400fe498d722e0fc
Onchain-Martinz <iwualakizitto@gmail.com>
2026-04-10 07:30:58 +0100

    Update AttributionSlide.tsx to add Simon as a co-presenter with Twitter link

---
+++ AttributionSlide.tsx
@@ -0,0 +1,16 @@
+// AttributionSlide.tsx
+
+const AttributionSlide = () => {
+    return (
+        <div>
+            <h1>Attribution</h1>
+            <p>Co-Presenters:</p>
+            <ul>
+                <li>Martinz</li>
+                <li>Simon (<a href="https://x.com/thisis_simon?s=21">Twitter</a>)</li>
+            </ul>
+        </div>
+    );
+};
+
+export default AttributionSlide;
